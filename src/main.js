73b6e53338ccd9797a2da6c7d0d8bb6157be6a91
zkunsang <zkunsang@gmail.com>
2019-12-29 10:45:42 +0900

    vue-router-test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+const Login = {template: '<div>Login Page</div>'}
+
+const routes = {
+  '/': App,
+  '/login': Login
+}
+
 new Vue({
   el: '#app',
-  render: h => h(App)
+  computed: {
+    VueComponent1() {
+      console.log(window.location);
+      return routes[window.location.pathname] || {template: '<div>Page not found</div>'}
+    }
+  },
+  render(h) {
+    return h(this.VueComponent1)
+  }
 })
