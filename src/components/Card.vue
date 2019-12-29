a516b33a2de43faa4c32004c17d4e3e5da0b1b79
zkunsang <zkunsang@gmail.com>
2019-12-29 12:00:08 +0900

    lecture6

---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        Card
+        {{cid}}
+    </div>
+</template>
+
+<script>
+export default {
+    created() {
+        this.cid = this.$route.params.cid
+        console.log(this.$route.params);
+    },
+    watch: {
+        '$route'() {
+            this.cid = this.$route.params.cid
+        }
+    },
+    data() {
+        return {
+            cid: 0
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
