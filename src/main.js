1b82382cefb41d82b960490b4068f09b56b5361b
zkunsang <zkunsang@gmail.com>
2019-12-29 11:15:38 +0900

    lecture4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import route from './router'
+import App from './App.vue'
 
 new Vue({
   el: '#app',
   router: route,
-  render: h => h({template: '<router-view></router-view>'})
+  render: h => h(App)
 })
