7e9d6e5924b45b8350cd913e70c9694bf7b3d097
zkunsang <zkunsang@gmail.com>
2019-12-29 12:00:00 +0900

    lecture6

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,6 +2,11 @@
     <div>
         Board
         {{bid}}
+        <router-link :to="`/b/${bid}/c/1`">Card 1</router-link>
+        <router-link :to="`/b/${bid}/c/2`">Card 2</router-link>
+        <hr />
+
+        <router-view></router-view>
     </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
 import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter);
@@ -10,7 +11,12 @@ Vue.use(VueRouter);
 const routes = [
   {path: '/', component: Home},
   {path: '/login', component: Login},
-  {path: '/b/:bid', component: Board},
+  {
+    path: '/b/:bid', component: Board,
+    children: [
+      {path: 'c/:cid', component: Card}
+    ]
+  },
   {path: '*', component: NotFound}
 ]
 
